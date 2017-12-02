18618d76b0196aec297b17829797bad0ed0e1aa5
Stone Cong <cong@dropbox.com>
2017-12-02 12:19:05 -0800

    Finish part 2 for day 5 of 2016

--- advent_of_code/2016/day05/test.cc
+++ advent_of_code/2016/day05/test.cc
@@ -1,13 +1,11 @@
 #include "../../../include/txt_file.hpp"
 #include "md5.h"
 
-int main()
+string get_code_part_1(string base)
 {
-  // http://www.zedwood.com/article/cpp-md5-function
-  // g++ test.cc md5.cpp -o test.out && ./test.out
-  string base = "ffykfhsq";
   string hash_result;
   string code;
+
   for (int i = 0; i < 99999999; i++) {
     string text = base + to_string(i);
     hash_result = md5(text.c_str());
@@ -23,8 +21,54 @@ int main()
     code += hash_result[5];
     if (code.size() == 8) break;
   }
+  return code;
+}
 
-  printf("code = %s\n", code.c_str());
+string get_code_part_2(string base)
+{
+  string hash_result;
+  string code(8, 'x');
+  for (int i = 0; i < 99999999; i++) {
+    string text = base + to_string(i);
+    hash_result = md5(text.c_str());
+    bool found = true;
+    for (int j = 0; j < 5; j++) {
+      if (hash_result[j] != '0') {
+        found = false;
+        break;
+      }
+    }
+    if (!found) continue;
+    printf("Result (%s) = %s\n", text.c_str(), hash_result.c_str());
+    int position = hash_result[5] - '0';
+    if (position < 0 || position > 7) {
+      continue;
+    }
+    if (code[position] != 'x') continue;
+    code[position] = hash_result[6];
+    found = false;
+    for (auto e: code) {
+      if (e == 'x') {
+        found = true;
+      }
+    }
+    if (!found) break;
+  }
+  return code;
+}
+
+int main()
+{
+  // http://www.zedwood.com/article/cpp-md5-function
+  // g++ test.cc md5.cpp -o test.out && ./test.out
+  string base = "ffykfhsq";
+  string code;
+
+  code = get_code_part_1(base);
+  printf("part 1: code = %s\n", code.c_str());
+
+  code = get_code_part_2(base);
+  printf("part 2: code = %s\n", code.c_str());
 
   return 0;
 }

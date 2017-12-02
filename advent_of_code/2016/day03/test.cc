c0db9f95988af147ad6ab09c0c6a73d436dfd9e2
Stone Cong <cong@dropbox.com>
2017-12-02 00:38:38 -0800

    Check in the code for advent of code 2016 day 3

--- advent_of_code/2016/day03/test.cc
+++ advent_of_code/2016/day03/test.cc
@@ -1,12 +1,66 @@
-#include <assert.h>
-#include <stdio.h>
+#include "../../../include/txt_file.hpp"
+#include <algorithm>
 
-#include <vector>
-#include <string>
+void print_vector(vector<int>& numbers)
+{
+  printf("v(%d):", (int)numbers.size());
+  for (auto e: numbers) {
+    printf(" %d", e);
+  }
+  printf("\n");
+}
 
-using namespace std;
+bool is_valid_triangle(vector<int> sizes)
+{
+  sort(sizes.begin(), sizes.end());
+  return sizes[0] + sizes[1] > sizes[2];
+}
+
+vector<vector<int>> transform_lines(vector<vector<int>>& lines)
+{
+  vector<vector<int>> result;
+
+  for (int i = 0; i < lines.size(); i += 3) {
+    for (int j = 0; j < 3; j++) {
+      vector<int> triangle;
+      for (int k = 0; k < 3; k++) {
+        triangle.push_back(lines[i + k][j]);
+      }
+      result.push_back(triangle);
+    }
+  }
+
+  return result;
+}
 
 int main()
 {
+  vector<string> raw_lines = get_lines("data.txt");
+  vector<vector<int>> lines;
+  for (auto e: raw_lines) {
+    vector<int> numbers = get_numbers(e);
+    lines.push_back(numbers);
+  }
+
+  int count = 0;
+  for (auto e: lines) {
+    if (is_valid_triangle(e)) {
+      ++count;
+    }
+  }
+
+  printf("Result: %d\n", count);
+
+  vector<vector<int>> new_lines = transform_lines(lines);
+
+  count = 0;
+  for (auto e: new_lines) {
+    if (is_valid_triangle(e)) {
+      ++count;
+    }
+  }
+
+  printf("Result: %d\n", count);
+
   return 0;
 }
---
+++ include/txt_file.hpp
@@ -0,0 +1,81 @@
+
+#include <assert.h>
+#include <stdio.h>
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// For example: get_lines("./test.txt")
+vector<string> get_lines(string path)
+{
+  vector<string> result;
+  ifstream infile(path);
+  string line;
+  while (getline(infile, line))
+  {
+    result.push_back(line);
+  }
+  return result;
+}
+
+bool is_num_digit(char c, int base)
+{
+  if (base <= 10) {
+    if (c < '0') return false;
+    return (c <= '0' + base - 1);
+  }
+
+  assert(base > 10 && base <= 16);
+  if (c <= '9' && c >= '0') return true;
+  return (c <= 'A' + base - 11 && c >= 'A');
+}
+
+int get_digit(char c, int base)
+{
+  assert(is_num_digit(c, base));
+
+  if (base <= 10) {
+    return c - '0';
+  }
+
+  assert(base > 10 && base <= 16);
+  if (c <= '9' && c >= '0') return c - '0';
+  return c - 'A' + 10;
+}
+
+vector<int> get_numbers(string line, int base=10)
+{
+  vector<int> numbers;
+
+  int sum = 0;
+  bool processing_number = false;
+  for (int i = 0; i < line.size(); i++) {
+    char c = line[i];
+    if (!processing_number) {
+      if (is_num_digit(c, base)) {
+        processing_number = true;
+        sum = get_digit(c, base);
+      }
+      continue;
+    }
+
+    if (is_num_digit(c, base)) {
+      sum *= base;
+      sum += get_digit(c, base);
+      continue;
+    }
+
+    processing_number = false;
+    numbers.push_back(sum);
+  }
+
+  if (processing_number) {
+    numbers.push_back(sum);
+  }
+
+  return numbers;
+}

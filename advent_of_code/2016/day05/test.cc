5168c7431f93bdc281de4f755ddef5f3f59f43bc
Stone Cong <cong@dropbox.com>
2017-12-02 11:59:22 -0800

    Remove test code

--- advent_of_code/2016/day05/test.cc
+++ advent_of_code/2016/day05/test.cc
@@ -5,26 +5,22 @@ int main()
 {
   // http://www.zedwood.com/article/cpp-md5-function
   // g++ test.cc md5.cpp -o test.out && ./test.out
-  string result = md5("abc3231929");
-  printf("Result = %s\n", result.c_str());
-  result = md5("abc5017308");
-  printf("Result = %s\n", result.c_str());
-
-  string code;
   string base = "ffykfhsq";
+  string hash_result;
+  string code;
   for (int i = 0; i < 99999999; i++) {
     string text = base + to_string(i);
-    result = md5(text.c_str());
+    hash_result = md5(text.c_str());
     bool found = true;
     for (int j = 0; j < 5; j++) {
-      if (result[j] != '0') {
+      if (hash_result[j] != '0') {
         found = false;
         break;
       }
     }
     if (!found) continue;
-    printf("Result (%s) = %s\n", text.c_str(), result.c_str());
-    code += result[5];
+    printf("Result (%s) = %s\n", text.c_str(), hash_result.c_str());
+    code += hash_result[5];
     if (code.size() == 8) break;
   }
 

11dcd2c9a867ca720785a5197f5b892ab6fd8d62
Stone Cong <cong@dropbox.com>
2017-12-03 17:30:29 -0800

    Temp checkin for part 2 of day 10

--- advent_of_code/2016/day09/test.cc
+++ advent_of_code/2016/day09/test.cc
@@ -42,13 +42,68 @@ string decompress(string input)
   return result;
 }
 
-int main()
+bool contain_marker(string input)
+{
+  for (auto e: input) {
+    if (e == '(') return true;
+  }
+  return false;
+}
+
+void part_1()
 {
   vector<string> data = get_lines();
   assert (data.size() == 1);
-  string input = data[0];
-  // cout << input << "\n";
-  string result = decompress(input);
-  cout << result.size() << ": "<< result << "\n";
+  cout << decompress(data[0]).size() << "\n";
+}
+
+void construct_initial_data(const string& input, unordered_map<pair<int, int>, string, PairHasher>& data, int& file_size)
+{
+  string result;
+  int current = 0;
+  /*
+  while (current < input.size()) {
+    char c = input[current];
+    if (c != '(') {
+      result.append(1, c);
+      current++;
+      continue;
+    }
+    string s;
+    int count;
+    get_marker(input, current, s, count);
+    for (int i = 0; i < count; i++) {
+      result += s;
+    }
+  }
+  return result;
+  */
+}
+
+long long get_length(string input)
+{
+  long long length = 0;
+  unordered_map<pair<int, int>, string, PairHasher> data;
+  //  track the markers and eof using a dict instead of saving the raw data
+  // (6x6)AFPLBX(2x3)ZE(53x13)(4x7)ZGQO(2x4)
+  // TBD: construct the initial dict to track all the markers and then feed it
+  //      to a loop until all the markers are resolved
+  int file_size = 0;
+  construct_initial_data(input, data, file_size);
+  return length;
+}
+
+void part_2()
+{
+  vector<string> data = get_lines();
+  assert (data.size() == 1);
+  cout << "part 2 result: " << get_length(data[0]) << "\n";
+  // if (!contain_marker(new_result)) break;
+}
+
+int main()
+{
+  // part_1();
+  part_2();
   return 0;
 }

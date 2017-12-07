8d6e550dc3cd1d819241a46b037422e6ca71aa0b
Stone Cong <cong@dropbox.com>
2017-12-06 22:25:38 -0800

    Check in code for day 7 for year 2017

--- advent_of_code/2015/day-06/test.cc
+++ advent_of_code/2015/day-06/test.cc
@@ -1,7 +1,33 @@
 #include "../../../include/txt_file.hpp"
 
+enum instruction {
+  turn_on = 1,
+  toggle = 2,
+  turn_off = 4
+};
+
+instruction get_instruction(string line)
+{
+  if (line.find("turn on") != string::npos) return turn_on;
+  if (line.find("toggle") != string::npos) return toggle;
+  assert(line.find("turn on") != string::npos);
+  return turn_off;
+}
+
 int main()
 {
   vector<string> data = get_lines();
+  map<pair<int, int>, int> dict;
+  for (auto e: data) {
+    switch (get_instruction(e)) {
+    case turn_on:
+      break;
+    case toggle:
+      break;
+    case turn_off:
+      break;
+    }
+  }
+
   return 0;
 }

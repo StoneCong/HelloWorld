3f404d143e1ef20f423414156103316f147de58e
Stone Cong <cong@dropbox.com>
2017-12-06 23:50:15 -0800

    Complete day 6 of 2015

--- advent_of_code/2015/day-06/test.cc
+++ advent_of_code/2015/day-06/test.cc
@@ -10,24 +10,96 @@ instruction get_instruction(string line)
 {
   if (line.find("turn on") != string::npos) return turn_on;
   if (line.find("toggle") != string::npos) return toggle;
-  assert(line.find("turn on") != string::npos);
+  if (line.find("turn off") != string::npos) return turn_off;
+  assert(false); // should not be here!
   return turn_off;
 }
 
-int main()
+void part_1()
 {
   vector<string> data = get_lines();
   map<pair<int, int>, int> dict;
   for (auto e: data) {
-    switch (get_instruction(e)) {
-    case turn_on:
-      break;
-    case toggle:
-      break;
-    case turn_off:
-      break;
+    vector<int> numbers = get_numbers(e);
+    assert(numbers.size() == 4);
+    instruction inst = get_instruction(e);
+    for (int row = numbers[0]; row <= numbers[2]; row++) {
+      for (int col = numbers[1]; col <= numbers[3]; col++) {
+        switch (inst) {
+        case turn_on:
+          dict[make_pair(row, col)] = 1;
+          break;
+        case toggle:
+          if (dict.find(make_pair(row, col)) != dict.end()) {
+            dict[make_pair(row, col)] = dict[make_pair(row, col)] ? 0 : 1;
+          } else {
+            dict[make_pair(row, col)] = 1;
+          }
+          break;
+        case turn_off:
+          dict[make_pair(row, col)] = 0;
+          break;
+        }
+      }
     }
   }
 
+  int count = 0;
+  for (auto e: dict) {
+    if (e.second) count++;
+  }
+
+  cout << "number of lights lit: " << count << endl;
+
+}
+
+void part_2()
+{
+  vector<string> data = get_lines();
+  map<pair<int, int>, int> dict;
+  for (auto e: data) {
+    vector<int> numbers = get_numbers(e);
+    assert(numbers.size() == 4);
+    instruction inst = get_instruction(e);
+    for (int row = numbers[0]; row <= numbers[2]; row++) {
+      for (int col = numbers[1]; col <= numbers[3]; col++) {
+        switch (inst) {
+        case turn_on:
+          if (dict.find(make_pair(row, col)) != dict.end()) {
+            dict[make_pair(row, col)] += 1;
+          } else {
+            dict[make_pair(row, col)] = 1;
+          }
+          break;
+        case toggle:
+          if (dict.find(make_pair(row, col)) != dict.end()) {
+            dict[make_pair(row, col)] += 2;
+          } else {
+            dict[make_pair(row, col)] = 2;
+          }
+          break;
+        case turn_off:
+          if (dict.find(make_pair(row, col)) != dict.end()) {
+            if (dict[make_pair(row, col)]) {
+              dict[make_pair(row, col)] -= 1;
+            }
+          }
+          break;
+        }
+      }
+    }
+  }
+
+  int count = 0;
+  for (auto e: dict) {
+    count += e.second;
+  }
+
+  cout << "total brightness: " << count << endl;
+}
+
+int main()
+{
+  part_2();
   return 0;
 }

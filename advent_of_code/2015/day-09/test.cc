0d239adc038b114de147d6c979a4938e344ba501
Stone Cong <cong@Graces-MacBook-Air.local>
2017-12-09 12:10:11 -0800

    Part 1 of day 9 of 2015

---
+++ advent_of_code/2015/day-09/test.cc
@@ -0,0 +1,51 @@
+#include "../../../include/txt_file.hpp"
+
+void add_city(string city, vector<string>& cities)
+{
+  for (auto e: cities) {
+    if (e == city) return;
+  }
+  cities.push_back(city);
+}
+
+int get_path_cost(vector<int>& permutation, const vector<string>& cities, const map<pair<string, string>, int>& dict)
+{
+  int cost = 0;
+  for (int i = 0; i < permutation.size() - 1; i++) {
+    pair<string, string> current_pair = make_pair(cities[permutation[i]], cities[permutation[i + 1]]);
+    if (dict.find(current_pair) == dict.end()) {
+      return INT_MAX;
+    }
+    cost += dict.find(current_pair)->second;
+  }
+  return cost;
+}
+
+int get_shortest_route(const vector<string>& cities, const map<pair<string, string>, int>& dict)
+{
+  // get all the permutations of cities and check which one is shortest
+  vector<vector<int>> permutations = get_permutations(cities.size());
+  int shortest_route = INT_MAX;
+  for (auto p: permutations) {
+    int cost = get_path_cost(p, cities, dict);
+    shortest_route = min(shortest_route, cost);
+  }
+  return shortest_route;
+}
+
+int main(int arg, const char* argv[])
+{
+  vector<string> data = get_lines(arg == 1 ? "data.txt" : "real.txt");
+  map<pair<string, string>, int> dict;
+  vector<string> cities;
+  for (auto e: data) {
+    vector<int> numbers = get_numbers(e);
+    vector<string> words = get_words(e);
+    dict[make_pair(words[0], words[2])] = numbers[0];
+    dict[make_pair(words[2], words[0])] = numbers[0];
+    add_city(words[0], cities);
+    add_city(words[2], cities);
+  }
+  cout << get_shortest_route(cities, dict) << endl;
+  return 0;
+}

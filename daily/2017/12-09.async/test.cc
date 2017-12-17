069016826e77134a9bb4e854777ab18d2999838c
Stone Cong <stonecong@gmai.com>
2017-12-17 14:38:56 -0800

    Learn about how to use lambda to do the async call

--- daily/2017/12-09.async/test.cc
+++ daily/2017/12-09.async/test.cc
@@ -4,16 +4,30 @@
   More info: https://solarianprogrammer.com/2012/10/17/cpp-11-async-tutorial/
 */
 
-
 void called_from_async() {
   cout << "Message from async call!\n";
 }
 
-int main() {
+void simple()
+{
   //called_from_async launched in a separate thread if possible
   future<void> result(async(called_from_async));
   cout << "Message from main!\n";
   //ensure that called_from_async is launched synchronously
   //if it wasn't already launched
   result.get();
+}
+
+void lambda()
+{
+  //called_from_async launched in a separate thread if possible
+  future<int> result(async([](int m, int n) {return m + n;} , 2, 4));
+  cout << "Message from main!\n";
+  //retrive and print the value stored in the future
+  cout << result.get() << endl;
+}
+
+int main() {
+  simple();
+  lambda();
 }

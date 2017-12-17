a963925559d92a05bef16b85dd3905b08b770f32
Stone Cong <stonecong@gmai.com>
2017-12-16 16:08:06 -0800

    More tests

--- daily/2017/12-10.lambda-expressions/test.cc
+++ daily/2017/12-10.lambda-expressions/test.cc
@@ -1,10 +1,36 @@
 #include "../../../include/txt_file.hpp"
 
-int main()
+void simple()
 {
   vector<int> v {-5, -19, 3, 10, 15, 20, 100};
   auto firstPos = find_if(begin(v), end(v), [](int n) {return n > 0;});
   if (firstPos != end(v)) {
     cout << "First positive value in v is: " << *firstPos << endl;
   }
+}
+
+void with_locals()
+{
+  vector<double> v {1.2, 4.7, 5, 9, 9.4};
+  double target = 4.9;
+  double epsilon = .3;
+
+  auto end_matches = partition(begin(v), end(v), [target, epsilon](double val){return fabs(target-val) < epsilon;});
+  cout << "Values within epsilon: " << for_each(begin(v), end_matches, [](double d){cout << d << ' ';});
+  cout << endl;
+}
+
+void func_within_func()
+{
+  double target = 4.9;
+  double epsilon = .3;
+  auto withinEpsilon = [=](double val) {return fabs(target - val) < epsilon; };
+  cout << (withinEpsilon(5.1) ? "Yes!" : "No!") << endl;
+}
+
+int main()
+{
+  simple();
+  with_locals();
+  func_within_func();
 }

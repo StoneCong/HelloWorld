dd81b684d1247a8b59760250367af574b5ad8c44
Stone Cong <stonecong@gmai.com>
2017-12-17 14:52:21 -0800

    More async sample code

--- daily/2017/12-09.async/test.cc
+++ daily/2017/12-09.async/test.cc
@@ -4,30 +4,69 @@
   More info: https://solarianprogrammer.com/2012/10/17/cpp-11-async-tutorial/
 */
 
+void print_seperator()
+{
+  cout << "==================================================" << endl;
+}
+
 void called_from_async() {
   cout << "Message from async call!\n";
 }
 
 void simple()
 {
-  //called_from_async launched in a separate thread if possible
+  print_seperator();
+  // Called_from_async launched in a separate thread if possible
   future<void> result(async(called_from_async));
   cout << "Message from main!\n";
-  //ensure that called_from_async is launched synchronously
-  //if it wasn't already launched
+  // Ensure that called_from_async is launched synchronously
+  // If it wasn't already launched
   result.get();
 }
 
 void lambda()
 {
-  //called_from_async launched in a separate thread if possible
+  print_seperator();
+  // called_from_async launched in a separate thread if possible.
+  // Note return type is int
   future<int> result(async([](int m, int n) {return m + n;} , 2, 4));
   cout << "Message from main!\n";
   //retrive and print the value stored in the future
   cout << result.get() << endl;
 }
 
+int twice(int m)
+{
+  return m * 2;
+}
+
+void multiple()
+{
+  print_seperator();
+  vector<future<int>> futures;
+  for (int i = 0; i < 10; i++) {
+    futures.push_back(async(twice, i));
+  }
+  for (auto& e: futures) {
+    cout << e.get() << endl;
+  }
+}
+
+void multiple_lambda()
+{
+  print_seperator();
+  vector<future<int>> futures;
+  for (int i = 0; i < 10; i++) {
+    futures.push_back(async([](int m){return m*2;}, i));
+  }
+  for (auto& e: futures) {
+    cout << e.get() << endl;
+  }
+}
+
 int main() {
   simple();
   lambda();
+  multiple();
+  multiple_lambda();
 }

fb787a9e7b601be03add4ce56c5b37bf55473643
Stone Cong <stonecong@gmail.com>
2017-12-27 17:05:48 -0800

    Check in test code for valgrind

---
+++ daily/2017/12-18.valgrind-mem-check/test.cc
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+  int * array;
+  array = (int *)malloc(sizeof(int) * 10);
+  printf("addr = %p\n", array);
+  // free(array);  <== cause memory leak!
+}
